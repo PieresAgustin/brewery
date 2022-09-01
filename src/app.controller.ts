1ceb9c425870a51cd445de6d87bb3c9857b5e49c
Agustin Pieres <pieresagustin@gmail.com>
2022-09-01 15:55:30 -0300

    Commit /beers added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/beers")
+  getbeers(): string {
+    return this.appService.getbeers();
+  }
 }
